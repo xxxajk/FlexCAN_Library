e8d6ddffb923483fb39375e7b600a121b10c1c43
Pawelsky <pawelsky@users.noreply.github.com>
2016-08-27 12:43:51 +0200

    Added support for second FlexCAN controller on MK66FX1M0

--- FlexCAN.cpp
+++ FlexCAN.cpp
@@ -1,6 +1,7 @@
 // -------------------------------------------------------------
 // a simple Arduino Teensy3.1 CAN driver
 // by teachop
+// dual CAN support for MK66FX1M0 by Pawelsky
 //
 #include "FlexCAN.h"
 #include "kinetis_flexcan.h"
@@ -9,54 +10,88 @@ static const int txb = 8; // with default settings, all buffers before this are 
 static const int txBuffers = 8;
 static const int rxb = 0;
 
+#define FLEXCANb_MCR(b)                   (*(vuint32_t*)(b))
+#define FLEXCANb_CTRL1(b)                 (*(vuint32_t*)(b+4))
+#define FLEXCANb_RXMGMASK(b)              (*(vuint32_t*)(b+0x10))
+#define FLEXCANb_IFLAG1(b)                (*(vuint32_t*)(b+0x30))
+#define FLEXCANb_CTRL2(b)                 (*(vuint32_t*)(b+0x34))
+#define FLEXCANb_RXFGMASK(b)              (*(vuint32_t*)(b+0x48))
+#define FLEXCANb_MBn_CS(b, n)             (*(vuint32_t*)(b+0x80+n*0x10))
+#define FLEXCANb_MBn_ID(b, n)             (*(vuint32_t*)(b+0x84+n*0x10))
+#define FLEXCANb_MBn_WORD0(b, n)          (*(vuint32_t*)(b+0x88+n*0x10))
+#define FLEXCANb_MBn_WORD1(b, n)          (*(vuint32_t*)(b+0x8C+n*0x10))
+#define FLEXCANb_IDFLT_TAB(b, n)          (*(vuint32_t*)(b+0xE0+(n*4)))
+
 // -------------------------------------------------------------
-FlexCAN::FlexCAN(uint32_t baud)
+FlexCAN::FlexCAN(uint32_t baud, uint8_t id)
 {
-  // set up the pins, 3=PTA12=CAN0_TX, 4=PTA13=CAN0_RX
-  CORE_PIN3_CONFIG = PORT_PCR_MUX(2);
-  CORE_PIN4_CONFIG = PORT_PCR_MUX(2);// | PORT_PCR_PE | PORT_PCR_PS;
+  flexcanBase = FLEXCAN0_BASE;
+#ifdef __MK66FX1M0__
+  if(id > 0) flexcanBase = FLEXCAN1_BASE;
+#endif
+
+  // set up the pins
+  if(flexcanBase == FLEXCAN0_BASE)
+  {
+    // 3=PTA12=CAN0_TX, 4=PTA13=CAN0_RX
+    CORE_PIN3_CONFIG = PORT_PCR_MUX(2);
+    CORE_PIN4_CONFIG = PORT_PCR_MUX(2);// | PORT_PCR_PE | PORT_PCR_PS;
+  }
+#ifdef __MK66FX1M0__
+  else if(flexcanBase == FLEXCAN1_BASE)
+  {
+    // 33=PTA12=CAN1_RX, 34=PTA13=CAN1_TX
+    CORE_PIN33_CONFIG = PORT_PCR_MUX(2);
+    CORE_PIN34_CONFIG = PORT_PCR_MUX(2);// | PORT_PCR_PE | PORT_PCR_PS;
+  }
+#endif
+
   // select clock source 16MHz xtal
   OSC0_CR |= OSC_ERCLKEN;
-  SIM_SCGC6 |=  SIM_SCGC6_FLEXCAN0;
-  FLEXCAN0_CTRL1 &= ~FLEXCAN_CTRL_CLK_SRC;
+  if(flexcanBase == FLEXCAN0_BASE) SIM_SCGC6 |=  SIM_SCGC6_FLEXCAN0;
+#ifdef __MK66FX1M0__
+  else if(flexcanBase == FLEXCAN1_BASE) SIM_SCGC3 |=  SIM_SCGC3_FLEXCAN1;
+#endif
+
+  FLEXCANb_CTRL1(flexcanBase) &= ~FLEXCAN_CTRL_CLK_SRC;
 
   // enable CAN
-  FLEXCAN0_MCR |=  FLEXCAN_MCR_FRZ;
-  FLEXCAN0_MCR &= ~FLEXCAN_MCR_MDIS;
-  while(FLEXCAN0_MCR & FLEXCAN_MCR_LPM_ACK)
+  FLEXCANb_MCR(flexcanBase) |=  FLEXCAN_MCR_FRZ;
+  FLEXCANb_MCR(flexcanBase) &= ~FLEXCAN_MCR_MDIS;
+  while(FLEXCANb_MCR(flexcanBase) & FLEXCAN_MCR_LPM_ACK)
     ;
   // soft reset
-  FLEXCAN0_MCR ^=  FLEXCAN_MCR_SOFT_RST;
-  while(FLEXCAN0_MCR & FLEXCAN_MCR_SOFT_RST)
+  FLEXCANb_MCR(flexcanBase) ^=  FLEXCAN_MCR_SOFT_RST;
+  while(FLEXCANb_MCR(flexcanBase) & FLEXCAN_MCR_SOFT_RST)
     ;
   // wait for freeze ack
-  while(!(FLEXCAN0_MCR & FLEXCAN_MCR_FRZ_ACK))
+  while(!(FLEXCANb_MCR(flexcanBase) & FLEXCAN_MCR_FRZ_ACK))
     ;
   // disable self-reception
-  FLEXCAN0_MCR |= FLEXCAN_MCR_SRX_DIS;
+  FLEXCANb_MCR(flexcanBase) |= FLEXCAN_MCR_SRX_DIS;
 
   //enable RX FIFO
-  FLEXCAN0_MCR |= FLEXCAN_MCR_FEN;
+  FLEXCANb_MCR(flexcanBase) |= FLEXCAN_MCR_FEN;
 
   // segment splits and clock divisor based on baud rate
-  if ( 50000 == baud ) {
-    FLEXCAN0_CTRL1 = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
-                      | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(19));
+    if ( 50000 == baud ) {
+    FLEXCANb_CTRL1(flexcanBase) = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
+                                | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(19));
   } else if ( 100000 == baud ) {
-    FLEXCAN0_CTRL1 = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
-                      | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(9));
+    FLEXCANb_CTRL1(flexcanBase) = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
+                                | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(9));
   } else if ( 250000 == baud ) {
-    FLEXCAN0_CTRL1 = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
-                      | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(3));
+    FLEXCANb_CTRL1(flexcanBase) = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
+                                | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(3));
   } else if ( 500000 == baud ) {
-    FLEXCAN0_CTRL1 = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
-                      | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(1));
+    FLEXCANb_CTRL1(flexcanBase) = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
+                                | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(1));
   } else if ( 1000000 == baud ) {
-    FLEXCAN0_CTRL1 = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(0)
-                      | FLEXCAN_CTRL_PSEG1(1) | FLEXCAN_CTRL_PSEG2(1) | FLEXCAN_CTRL_PRESDIV(1));
+    FLEXCANb_CTRL1(flexcanBase) = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(0)
+                                | FLEXCAN_CTRL_PSEG1(1) | FLEXCAN_CTRL_PSEG2(1) | FLEXCAN_CTRL_PRESDIV(1));
   } else { // 125000
-    FLEXCAN0_CTRL1 = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
-                      | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(7));
+    FLEXCANb_CTRL1(flexcanBase) = (FLEXCAN_CTRL_PROPSEG(2) | FLEXCAN_CTRL_RJW(1)
+                                | FLEXCAN_CTRL_PSEG1(7) | FLEXCAN_CTRL_PSEG2(3) | FLEXCAN_CTRL_PRESDIV(7));
   }
 
   // Default mask is allow everything
@@ -70,8 +105,8 @@ FlexCAN::FlexCAN(uint32_t baud)
 void FlexCAN::end(void)
 {
   // enter freeze mode
-  FLEXCAN0_MCR |= (FLEXCAN_MCR_HALT);
-  while(!(FLEXCAN0_MCR & FLEXCAN_MCR_FRZ_ACK))
+  FLEXCANb_MCR(flexcanBase) |= (FLEXCAN_MCR_HALT);
+  while(!(FLEXCANb_MCR(flexcanBase) & FLEXCAN_MCR_FRZ_ACK))
     ;
 }
 
@@ -79,26 +114,26 @@ void FlexCAN::end(void)
 // -------------------------------------------------------------
 void FlexCAN::begin(const CAN_filter_t &mask)
 {
-  FLEXCAN0_RXMGMASK = 0;
+  FLEXCANb_RXMGMASK(flexcanBase) = 0;
 
   //enable reception of all messages that fit the mask
   if (mask.ext) {
-    FLEXCAN0_RXFGMASK = ((mask.rtr?1:0) << 31) | ((mask.ext?1:0) << 30) | ((mask.id & FLEXCAN_MB_ID_EXT_MASK) << 1);
+    FLEXCANb_RXFGMASK(flexcanBase) = ((mask.rtr?1:0) << 31) | ((mask.ext?1:0) << 30) | ((mask.id & FLEXCAN_MB_ID_EXT_MASK) << 1);
   } else {
-    FLEXCAN0_RXFGMASK = ((mask.rtr?1:0) << 31) | ((mask.ext?1:0) << 30) | (FLEXCAN_MB_ID_IDSTD(mask.id) << 1);
+    FLEXCANb_RXFGMASK(flexcanBase) = ((mask.rtr?1:0) << 31) | ((mask.ext?1:0) << 30) | (FLEXCAN_MB_ID_IDSTD(mask.id) << 1);
   }
 
   // start the CAN
-  FLEXCAN0_MCR &= ~(FLEXCAN_MCR_HALT);
+  FLEXCANb_MCR(flexcanBase) &= ~(FLEXCAN_MCR_HALT);
   // wait till exit of freeze mode
-  while(FLEXCAN0_MCR & FLEXCAN_MCR_FRZ_ACK);
+  while(FLEXCANb_MCR(flexcanBase) & FLEXCAN_MCR_FRZ_ACK);
 
   // wait till ready
-  while(FLEXCAN0_MCR & FLEXCAN_MCR_NOT_RDY);
+  while(FLEXCANb_MCR(flexcanBase) & FLEXCAN_MCR_NOT_RDY);
 
   //set tx buffers to inactive
   for (int i = txb; i < txb + txBuffers; i++) {
-    FLEXCAN0_MBn_CS(i) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_INACTIVE);
+    FLEXCANb_MBn_CS(flexcanBase, i) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_INACTIVE);
   }
 }
 
@@ -108,9 +143,9 @@ void FlexCAN::setFilter(const CAN_filter_t &filter, uint8_t n)
 {
   if ( 8 > n ) {
     if (filter.ext) {
-      FLEXCAN0_IDFLT_TAB(n) = ((filter.rtr?1:0) << 31) | ((filter.ext?1:0) << 30) | ((filter.id & FLEXCAN_MB_ID_EXT_MASK) << 1);
+      FLEXCANb_IDFLT_TAB(flexcanBase, n) = ((filter.rtr?1:0) << 31) | ((filter.ext?1:0) << 30) | ((filter.id & FLEXCAN_MB_ID_EXT_MASK) << 1);
     } else {
-      FLEXCAN0_IDFLT_TAB(n) = ((filter.rtr?1:0) << 31) | ((filter.ext?1:0) << 30) | (FLEXCAN_MB_ID_IDSTD(filter.id) << 1);
+      FLEXCANb_IDFLT_TAB(flexcanBase, n) = ((filter.rtr?1:0) << 31) | ((filter.ext?1:0) << 30) | (FLEXCAN_MB_ID_IDSTD(filter.id) << 1);
     }
   }
 }
@@ -120,7 +155,7 @@ void FlexCAN::setFilter(const CAN_filter_t &filter, uint8_t n)
 int FlexCAN::available(void)
 {
   //In FIFO mode, the following interrupt flag signals availability of a frame
-  return (FLEXCAN0_IFLAG1 & FLEXCAN_IMASK1_BUF5M)? 1:0;
+  return (FLEXCANb_IFLAG1(flexcanBase) & FLEXCAN_IMASK1_BUF5M)? 1:0;
 }
 
 
@@ -140,15 +175,15 @@ int FlexCAN::read(CAN_message_t &msg)
   }
 
   // get identifier and dlc
-  msg.len = FLEXCAN_get_length(FLEXCAN0_MBn_CS(rxb));
-  msg.ext = (FLEXCAN0_MBn_CS(rxb) & FLEXCAN_MB_CS_IDE)? 1:0;
-  msg.id  = (FLEXCAN0_MBn_ID(rxb) & FLEXCAN_MB_ID_EXT_MASK);
+  msg.len = FLEXCAN_get_length(FLEXCANb_MBn_CS(flexcanBase, rxb));
+  msg.ext = (FLEXCANb_MBn_CS(flexcanBase, rxb) & FLEXCAN_MB_CS_IDE)? 1:0;
+  msg.id  = (FLEXCANb_MBn_ID(flexcanBase, rxb) & FLEXCAN_MB_ID_EXT_MASK);
   if(!msg.ext) {
     msg.id >>= FLEXCAN_MB_ID_STD_BIT_NO;
   }
 
   // copy out message
-  uint32_t dataIn = FLEXCAN0_MBn_WORD0(rxb);
+  uint32_t dataIn = FLEXCANb_MBn_WORD0(flexcanBase, rxb);
   msg.buf[3] = dataIn;
   dataIn >>=8;
   msg.buf[2] = dataIn;
@@ -157,7 +192,7 @@ int FlexCAN::read(CAN_message_t &msg)
   dataIn >>=8;
   msg.buf[0] = dataIn;
   if ( 4 < msg.len ) {
-    dataIn = FLEXCAN0_MBn_WORD1(rxb);
+    dataIn = FLEXCANb_MBn_WORD1(flexcanBase, rxb);
     msg.buf[7] = dataIn;
     dataIn >>=8;
     msg.buf[6] = dataIn;
@@ -171,7 +206,7 @@ int FlexCAN::read(CAN_message_t &msg)
   }
 
   //notify FIFO that message has been read
-  FLEXCAN0_IFLAG1 = FLEXCAN_IMASK1_BUF5M;
+  FLEXCANb_IFLAG1(flexcanBase) = FLEXCAN_IMASK1_BUF5M;
 
   return 1;
 }
@@ -187,7 +222,7 @@ int FlexCAN::write(const CAN_message_t &msg)
   // find an available buffer
   int buffer = -1;
   for ( int index = txb; ; ) {
-    if ((FLEXCAN0_MBn_CS(index) & FLEXCAN_MB_CS_CODE_MASK) == FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_INACTIVE)) {
+    if ((FLEXCANb_MBn_CS(flexcanBase, index) & FLEXCAN_MB_CS_CODE_MASK) == FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_INACTIVE)) {
       buffer = index;
       break;// found one
     }
@@ -205,20 +240,20 @@ int FlexCAN::write(const CAN_message_t &msg)
   }
 
   // transmit the frame
-  FLEXCAN0_MBn_CS(buffer) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_INACTIVE);
+  FLEXCANb_MBn_CS(flexcanBase, buffer) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_INACTIVE);
   if(msg.ext) {
-    FLEXCAN0_MBn_ID(buffer) = (msg.id & FLEXCAN_MB_ID_EXT_MASK);
+    FLEXCANb_MBn_ID(flexcanBase, buffer) = (msg.id & FLEXCAN_MB_ID_EXT_MASK);
   } else {
-    FLEXCAN0_MBn_ID(buffer) = FLEXCAN_MB_ID_IDSTD(msg.id);
+    FLEXCANb_MBn_ID(flexcanBase, buffer) = FLEXCAN_MB_ID_IDSTD(msg.id);
   }
-  FLEXCAN0_MBn_WORD0(buffer) = (msg.buf[0]<<24)|(msg.buf[1]<<16)|(msg.buf[2]<<8)|msg.buf[3];
-  FLEXCAN0_MBn_WORD1(buffer) = (msg.buf[4]<<24)|(msg.buf[5]<<16)|(msg.buf[6]<<8)|msg.buf[7];
+  FLEXCANb_MBn_WORD0(flexcanBase, buffer) = (msg.buf[0]<<24)|(msg.buf[1]<<16)|(msg.buf[2]<<8)|msg.buf[3];
+  FLEXCANb_MBn_WORD1(flexcanBase, buffer) = (msg.buf[4]<<24)|(msg.buf[5]<<16)|(msg.buf[6]<<8)|msg.buf[7];
   if(msg.ext) {
-    FLEXCAN0_MBn_CS(buffer) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_ONCE)
-                              | FLEXCAN_MB_CS_LENGTH(msg.len) | FLEXCAN_MB_CS_SRR | FLEXCAN_MB_CS_IDE;
+    FLEXCANb_MBn_CS(flexcanBase, buffer) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_ONCE)
+                                         | FLEXCAN_MB_CS_LENGTH(msg.len) | FLEXCAN_MB_CS_SRR | FLEXCAN_MB_CS_IDE;
   } else {
-    FLEXCAN0_MBn_CS(buffer) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_ONCE)
-                              | FLEXCAN_MB_CS_LENGTH(msg.len);
+    FLEXCANb_MBn_CS(flexcanBase, buffer) = FLEXCAN_MB_CS_CODE(FLEXCAN_MB_CODE_TX_ONCE)
+                                         | FLEXCAN_MB_CS_LENGTH(msg.len);
   }
 
   return 1;
